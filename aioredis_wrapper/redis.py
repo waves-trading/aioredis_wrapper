3922e17debf3b3c26ce5f11d1aa495fde152832c
salexs95 <salexs95@yandex.ru>
2021-03-04 21:08:32 +0300

    Prepare to test

--- aioredis_wrapper/redis.py
+++ aioredis_wrapper/redis.py
@@ -10,18 +10,35 @@ class RedisConnection(object):
     Class for async redis connection context manager.
     """
 
-    def __init__(self, *_, host: str = "127.0.0.1", port: int = 6379, password: Optional[str] = None, **__) -> None:
+    def __init__(
+            self,
+            *_,
+            host: str = "127.0.0.1",
+            port: int = 6379,
+            password: Optional[str] = None,
+            db: int = 0,
+            request_timeout: int = 2,
+            **__
+    ) -> None:
         self._host = host
         self._port = port
         self._password = password
         self._redis = None
+        self._timeout = request_timeout
+        self._db = db
 
     @property
     def address(self):
         return self._host, self._port
 
     async def __aenter__(self):
-        self._redis = await create_redis((self._host, self._port), password=self._password, encoding="utf-8")
+        self._redis = await create_redis(
+            (self._host, self._port),
+            db=self._db,
+            password=self._password,
+            encoding="utf-8",
+            timeout=self._timeout
+        )
         return self._redis
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
